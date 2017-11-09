9bc87f9a83b18034ccc093c47c920e1ea43f3a31
Ashley <ASHL0005@e.ntu.edu.sg>
2017-11-10 04:28:05 +0800

    ASDF

--- src/moblima/MovieApp.java
+++ src/moblima/MovieApp.java
@@ -1,10 +1,13 @@
 package moblima;
 
+import java.util.Scanner;
+
 public class MovieApp {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		int choice;
+		boolean adminLogin = false;
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Are you an admin or user?");
@@ -13,39 +16,49 @@ public class MovieApp {
 		System.out.println("Enter your choice: ");
 		choice = sc.nextInt();
 		
-		switch (choice)
+		switch (choice) {
 		case 1:
+			AdminController adminController = new AdminController();	
+			adminLogin = adminController.login();
+			while (choice != 9 && adminLogin == true) {
+			System.out.println(
+			        "=========Admin Menu========\n" +
+			        "(1) Create movie listing\n" +
+			        "(2) Update movie listing\n" +
+			        "(3) Remove movie listing\n" +
+			        "(4) Create cinema showtime\n" +
+			        "(5) Update cinema showtime\n" +
+			        "(6) Remove cinema showtime\n" +
+			        "(7) Change movie base price\n" +
+			        "(8) Logout\n" +
+			        "(9) Exit MOBLIMA");   
+			System.out.println("Enter your choice: ");
+			choice = sc.nextInt();
+			}
 			break;
 		case 2:	
 			do {
-				System.out.println("(1) List all movies");
-				System.out.println("(2) Search movie details");
-				System.out.println("(3) Check seat availability");
-				System.out.println("(4) Book and purchase ticket");
-				System.out.println("(5) View booking history");
-				System.out.println("(6) List the Top 5 rankings by sale");
-				System.out.println("(7) List the Top 5 rankings by rating");
-				System.out.println("(8) Exit MOBLIMA");
+				System.out.println(
+						"=========User Menu========\n" +
+				        "(1) List all movies\n" +
+				        "(2) Search movie details\n" +
+				        "(3) Book and purchase ticket\n" +
+				        "(4) View booking history\n" +
+				        "(5) Add movie review\n" +
+				        "(6) List Top 5 rankings by sale\n" +
+				        "(7) List Top 5 rankings by rating\n" +
+				        "(8) Exit MOBLIMA");  
 				System.out.println("Enter your choice: ");
 				choice = sc.nextInt();
-				switch (choice) {
-				case 1:
-					
-				}
-			} while (choice != 7);
+			} while (choice != 8);
 			break;
 		default:
 				System.out.println("Invalid choice!");
 		}
+}
 		
-=======
-		// TODO Auto-generated method stub
-// Ashley made this
-		//Ada was here
-		//aba
-		//
-		//Qinan was here
-		BookingDatabaseController controller = new BookingDatabaseController();
+
+		/*BookingDatabaseController controller = new BookingDatabaseController();
 		Booking test1 = new Booking(controller.generateBookingID(), (double)92739376, "jiahengzhang@mensa.org.sg", (double)100000, (double)123456, "Febuary 2nd", 10002);
 		System.out.println(test1.getBookingID() + test1.getMobileNum() + test1.getEmail() + test1.getMovieID() + test1.getCinemaCode() + test1.getTicketID() + test1.getDateTime());
 
@@ -53,8 +66,8 @@ public class MovieApp {
 
 
 		controller.addBookingIntoDatabase(test1);
-		controller.printBookingHistory(5);
+		controller.printBookingHistory(5);*/
 
 	}
 
-}
+
