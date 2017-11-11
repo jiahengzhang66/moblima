ca87c008a5f7c0789cb2621a20045c150809d2ff
AdaTanYD <asus@10.27.169.123>
2017-11-11 13:25:21 +0800

    test

--- src/moblima/MovieApp.java
+++ src/moblima/MovieApp.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MovieApp {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		// TODO Auto-generated method stub
 // Ashley made this
 		//Ada was here
@@ -14,7 +14,6 @@ public class MovieApp {
 		//Qinan was here
 		BookingDatabaseController controller = new BookingDatabaseController();
 		Booking test1 = new Booking(controller.generateBookingID(), (double)92739376, "jiahengzhang66@gmail.com.sg", (double)100000, (double)123456, "Febuary 2nd", 10002);
-=======
 
 		int choice;
 		boolean adminLogin = false;
