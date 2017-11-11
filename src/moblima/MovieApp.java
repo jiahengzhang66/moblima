ce7d7433525d5ebfaac9be73c195abb15e0f098c
sunqi <sunqi@10.27.244.40>
2017-11-11 13:35:18 +0800

    qinan

--- src/moblima/MovieApp.java
+++ src/moblima/MovieApp.java
@@ -12,6 +12,7 @@ public class MovieApp {
 		//aba
 		//
 		//Qinan was here
+// qinan
 		BookingDatabaseController controller = new BookingDatabaseController();
 		Booking test1 = new Booking(controller.generateBookingID(), (double)92739376, "jiahengzhang66@gmail.com.sg", (double)100000, (double)123456, "Febuary 2nd", 10002);
 =======
